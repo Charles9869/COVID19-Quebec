f62d0548ebe6444b961eba38672fa7e7ab6fa919
Charles Bertrand <cbertrand2198@gmail.com>
2020-05-10 12:20:57 -0400

    fix some things

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-// const fetch = require('isomorphic-fetch');
 const cheerio = require('cheerio');
 const request = require('request');
 const cors = require('cors');
